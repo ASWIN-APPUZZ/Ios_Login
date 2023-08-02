a1347e6339a61a100f3e5a19c9c445fec624ffd2
aswin <aswin0484@gmail.com>
2023-08-02 14:39:21 +0530

    First commit

--- DEMO/ViewController.swift
+++ DEMO/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailid: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var labeltext: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        let email = emailid.text
+        let pass = password.text
+        
+        if(email == "Aswin" && pass == "123"){
+            labeltext.text = "vaild"
+        }else{
+            labeltext.text = "Invaild"
+        }
+        
+    }
+    
 }
 
